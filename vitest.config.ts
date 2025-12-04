6fbaa0652917a46583c28eab78c419adb0d393f0
Joe Mou <dev@mou.fo>
2025-12-04 00:25:18 -0500

    Fix vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config.js";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: false,
     environment: "node",
+    include: ["src/**/*.test.ts"],
   },
 });
